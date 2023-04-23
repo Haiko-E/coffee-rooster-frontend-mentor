2b5ca480eb7e93fca7be1fd34cdd2b2ed64367f1
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2023-04-23 22:10:36 +0200

    added my favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,9 +5,6 @@ import Head from 'next/head';
 export default function Layout({ children, className }) {
   return (
     <>
-      <Head>
-        <link rel='favicon Haiko' href='/favicon.svg' />
-      </Head>
       <Navbar className={className} />
       <main>{children}</main>
       <Footer className={className} />
