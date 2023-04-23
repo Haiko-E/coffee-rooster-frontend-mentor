33069d6ae662571cbf4fa4c68d8703f6c19b245c
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2023-04-23 22:15:10 +0200

    added my favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,6 +5,9 @@ import Head from 'next/head';
 export default function Layout({ children, className }) {
   return (
     <>
+      <Head>
+        <link rel='shortcut icon' href='/images/favicon.svg' />
+      </Head>
       <Navbar className={className} />
       <main>{children}</main>
       <Footer className={className} />
